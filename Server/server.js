7e0ed0a2df9398d08d7509a68c50101bae9bd631
Douglas Romero <Douglas Romero_1984>
2026-05-01 21:07:13 -0300

    subir archivos del proyecto

--- Server/server.js
+++ Server/server.js
@@ -2,6 +2,21 @@
 //  SERVER.JS — Servidor principal con Socket.io
 // ════════════════════════════════════════════
 
+require('dotenv').config();
+
+const express    = require('express');
+const http       = require('http');
+const path       = require('path');
+const { Server } = require('socket.io');
+
+const app    = express();
+const server = http.createServer(app);
+const io     = new Server(server);
+
+// Ahora usás las variables así:
+const PORT = process.env.PORT || 3000;
+
+
 const express = require('express');
 const http    = require('http');
 const path    = require('path');
